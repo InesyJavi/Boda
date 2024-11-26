c33068542d93b65f5f1686e75340dda0d525eb44
albabel3 <albabel3@hotmail.com>
2024-11-26 23:36:30 +0100

    november

--- befca5b5ad21547240e8fc750a95df12f67c088c/script.js
+++ befca5b5ad21547240e8fc750a95df12f67c088c/script.js
@@ -16,68 +16,109 @@ Nos vemos el mes que viene con una sorpresa que tendrá un toque fresquito acord
   imagePath: "images/day2.jpg"
   },
   {
-  message: "Mes3",
-  comment: "Mes3",
+  message: `<p>¡¡Hola chicos!! Parece que fue anteayer, pero ya han pasado dos meses desde vuestra boda, [tercer día 28 desde vuestra boda incluyendo el propio día de la boda o segundo sin incluirlo, segundo mensiversario, primer QR (es siempre el mismo), tercera entrada de la página (que no "a la página", puesto que habéis entrado más veces), en resumen: acabáis de entrar a la página, después de muchas veces de entrar, para ver, por primera vez, la tercera entrada de la página]. Sí sí, estos textos los vamos escribiendo como va pasando el tiempo para que estén actualizados cuando los leáis.</p>
+<p>Antes de seguir leyendo, tened a mano la caja (¡LA CAJA, LA CAJA!) para abrirla cuando y sólo cuando este texto, que ahora tiene jurisdicción sobre vuestros actos, os lo indique.</p>
+<p>Noviembre es el mes de que se empiece a hacer de noche superpronto. Antes también era el mes en que llegaba el frío, pero ahora con el cambio climático simplemente sólo se hace de noche antes. Aun así, las costumbres no hay que perderlas aunque las condiciones climáticas cambien, así que podéis poner el aire acondicionado (o abrir las ventanas de casa durante unas 72 h para un enfriamiento de la casa más progresivo), elegir una de las pelis que os sugerimos a continuación y arroparos en el sofá para verla con la preciosa XXXXX (palabra censurada para no hacer espóiler de lo que hay en la caja) que os hemos regalado. Ahora ya podéis abrir la caja (¡LA CAJA, LA CAJA!).</p>`,
+  comment: `<p>Pelis en general:</p>
+<p>-The wild robot (la recomendó Alba)</p>
+<p>-Eduardo Manostijeras (clasicazo) [Disney+]</p>
+<p>-La llegada (no sé si la habrán visto, va de extraterrestres y una lingüista que intenta comunicarse con ellos, es rara pero es clásica) [Netflix]</p>
+<p>-La terminal</p>
+<p>-Princesa por sorpresa</p>
+<p>-Ahora me ves</p>
+
+<p>Musicales:</p>
+<p>-El gran showman</p>
+<p>-Moulin rouge</p>
+<p>-Into the woods (no recuerdo si a Inés le encanta o la odia)</p>
+<p>-La la land</p>
+
+<p>Navideñas tipo sobremesa de la 3:</p>
+<p>-Jingle all the way (un padre en apuros)</p>
+<p>-Solo en casa</p>
+<p>-Bad santa</p>
+
+<p>Comedias románticas:</p>
+<p>-The holiday</p>
+<p>-About Time</p>
+<p>-Last Christmas</p>
+<p>-Let it snow</p>
+<p>-Happiest Season</p>
+<p>-Single All the Way</p>
+<p>-Jingle Jangle: A Christmas Journey</p>
+<p>-The Knight Before Christmas</p>
+<p>-Nos vemos la próxima Navidad [Netflix] (es de 2024 y seguro que no la han visto)</p>
+<p>-Navidades en California</p>
+<p>-Un príncipe de Navidad</p>
+<p>-La típica Navidad</p>
+<p>-El caballero de la navidad</p>
+<p>-Noche de fin de año</p>
+<p>-Como perder a un hombre en 10 dias</p>
+
+<p>Animación:</p>
+<p>-Polar Express</p>
+<p>-Pesadilla antes de Navidad</p>
+<p>-Klaus</p>`,
   imagePath: "images/day3.jpg"
   },
   {
-  message: "We share the same values, but at the same time, we have different opinions. I love that we see things almost the same way, but we still disagree on things. It makes you so much more interesting, and I learn from you every day, making me a better person.",
-  comment: "01/09/2018 - From Zgz to Stras: You visited my region for the first time. We went to Strasbourg and had a lovely romantic week there. It's crazy to think that 5 years later, I would still be deeply in love with you and so happy.",
+  message: "Diciembre",
+  comment: "Diciembre",
   imagePath: "images/day4.jpg"
   },
   {
-  message: "You are so funny. You are the person I laughed the most with these past 5 years. I love how we have the same humor and that you accept my weirdness. Do you remember when I was chasing you, making a nerd voice in Glasgow, and we got stuck outside my flat? Hahahaha.",
-  comment: "23/03/2019 - A Step Closer: Unfortunately, I couldn't find an internship in Spain and had to go to Toulouse as a backup plan to get closer to you (lol, if only I knew it was the worst city when it comes to public transportation). I still have good memories of that period, mostly because even though we were still far away from each other, we were able to see each other more often and without planning it 3 months in advance. Also, I had the coolest flat I have ever lived in.",
+  message: "Enero",
+  comment: "Enero",
   imagePath: "images/day5.jpg"
   },
   {
-  message: "You are so good at making gifts, and you know how to make people feel so special (which I am not). I just found the 'Yacine's manual' you made me for our first birthday together! I laughed so much reading it again, and it made me want to cuddle you so much.",
-  comment: "17/08/2019 - Ich bin ein Berliner: Our first trip together in Berlin for your 24th birthday. As you remember, it didn't start as planned because you missed one flight and got overbooked on another... So you had to spend the night alone in an airport hotel for your birthday... But once you got there, we had such a wonderful weekend in the frenetic Berlin. I had booked a 5-star hostel thanks to Airbus' CE, and that was pretty cool, haha.",
+  message: "Febrero",
+  comment: "Febrero",
   imagePath: "images/day6.jpg"
   },
   {
-  message: "You are the most beautiful creature. I love everything from your hair to your toes. I feel so attracted to you and find you even more beautiful every day I spend with you. I am the luckiest man.",
-  comment: "27/06/2020 - Barna aka Barça: The beginning of a long love story with my second lover... the beautiful city of Barcelona. This photo is from when you visited me for the first time after COVID, and we had to fake some documentation to say you were living with me. Me moving to this city is a big milestone in our relationship. After 2 years of being together, we were finally able to see each other often. I remember feeling so happy to be able to take the AVE and be in Zaragoza in 1h23... Now that I was living in Spain, I had so many things to discover with you about your culture, the language, the nature, etc. Remember, at that time, I didn't speak a word of Spanish and still had doubts about the fact that there were cold places in Spain. You helped me and taught me so many things without judging my ignorance, and I thank you and your family for that. Living here made me realize how much I love Spain and its culture, and I hope one day we can go back to live there together.",
+  message: "Marzo",
+  comment: "Marzo",
   imagePath: "images/day7.jpg"
   },
   {
-  message: "You are a family person, and I love your family. I admire you for being so close and attentive to your family, and I hope I can be part of it one day.",
-  comment: "21/08/2020 - Is It a Bear?: I have such good memories of that unconventional trip. You know I love Eastern Europe, and I was happy to go to Romania with you. From the beautiful Transylvania and the Dracula stories to the Soviet city of Bucharest, we had fun all the way through. But please, when dogs attack you, don't walk away like it's normal.",
+  message: "Abril",
+  comment: "Abril",
   imagePath: "images/day8.jpg"
   },
   {
-  message: "You almost made me like cats. You might be the only person on earth able to make me like cats. You have this power over me. I feel that you can change me, and I like that :)",
-  comment: "07/03/2021 - Day Trip in Aragon: Just one of the many trips we had in Aragon. I don't think you realize how much I love those. From AlbaracAIN to Alquezar and the Aurin, your region is beautiful and full of diversity. I am so grateful that you and your family take the time to show me around and share your knowledge. You've passed your love of Aragon on to me.",
+  message: "Mayo",
+  comment: "Mayo",
   imagePath: "images/day9.jpg"
   },
   {
-  message: "I love your sensitivity even if you struggle with it sometimes. It makes you who you are, and even though it can be overwhelming sometimes, I think it makes you a better and stronger person.",
-  comment: "16/07/2021 - Hiii Haaa! Yo Soy Mexicano!: Coming out of the pandemic, I had to convince you to go to Mexico that summer, and I don't regret it at all. This is our first 'big' trip together outside of Europe, and my head is still filled with wonderful images from this part of the world. I loved everything: the food, the culture, the landscapes, the cenotes, the history, and the Maya temples. But I think what made this even more special was sharing the experience with you. We had so much fun fooling around in our tiny car, driving on the dangerous roads of Yucatan (be careful of the bumps!!!!), having endless conversations, and basically creating wonderful memories together.",
+  message: "Junio",
+  comment: "Junio",
   imagePath: "images/day10.jpg"
   },
   {
-  message: "Your voice is amazing. I always get emotional hearing you sing with your sister at Burns Night. Some of the most relaxing nights I've had were when you used to sing me to sleep in Glasgow. It made me fall for you even more.",
-  comment: "31/10/2021 - The Arab and the Witch: You're not kidding when it comes to costumes. This is one of the many nights out we've had together. I love going out with you and friends because it just feels so natural, and I always have a good time. PS: Omg, you look like a goddess in that costume!",
+  message: "Julio",
+  comment: "Juio",
   imagePath: "images/day11.jpg"
   },
   {
-  message: "I love your passion for the mountains and nature. I love that you can share that with me and teach me so many things about it. Spending time with you in nature is one of my favorite activities. I hope we can do that more in the future.",
-  comment: "07/04/2022 - Carla Is the Real Witch: You're not only loved by all my friends, but also my family. Seeing these photos of the two most important people in my life makes me realize how lucky I am. During that trip to Lanzarote, you also had the chance to get to know my friends from Uni better. And let's be honest, Carla is a b****, and everyone knows that.",
+  message: "Agosto",
+  comment: "Agosto",
   imagePath: "images/day12.jpg"
   },
   {
-  message: "I love that you are sociable and so likable. I am always so proud of presenting you to my friends and family. They all love you so much that it makes me love you even more.",
-  comment: "10/08/2022 - Bun Bo Huè: One of my best memories. This trip was just perfect, and I feel like Sarah and Minh are so compatible with us as a couple. From the weirdest hotel lost in the middle of the Vietnamese countryside (and your terrible night, hahahaha) to the 5-star hotel on a private island, and finally to Minh's amazing, kind, and generous family, this trip will always have a special place in my heart.",
+  message: "Septiembre",
+  comment: "Septiembre",
   imagePath: "images/day13.jpg"
   },
   {
-  message: "I love traveling with you. The last trips we had together were some of the best memories I have. You are so easygoing, and we understand each other so much that it flows so naturally. We have so much fun together.",
-  comment: "09/04/2023 - Our Friends: A rare photo of all our friends from Barcelona. They all love you so much, and they're so happy for us. I'll miss them, and I'm sure you will too, but we'll find a way to see them from time to time.",
+  message: "Fin",
+  comment: "Fin",
   imagePath: "images/day14.jpg"
   },
   {
-  message: "I am so happy you finally found a job that suits you and that they respect you as a human being. You deserve it so much! I would love to have you in my company.",
-  comment: "2024? - To the Next Destination: I didn't find a photo of us in Lyon, but I like this one. Just to say, I think we are ready to take our relationship to the next level. After all the memories we have together living apart from each other, I want to create new ones living by your side. I cannot wait to start our new life together in Lyon. I love you. PS: Check the website tomorrow, you never know ;)",
+  message: "Fin",
+  comment: "Fin",
   imagePath: "images/day15.jpg"
   }
   ];
@@ -91,6 +132,8 @@ Nos vemos el mes que viene con una sorpresa que tendrá un toque fresquito acord
   const prevDayButton = document.getElementById("prevDayBtn");
   const nextDayButton = document.getElementById("nextDayBtn");
   const currentDate = new Date();
+
+
   
   // Date de début : 28 septembre 2024
   const startDate = new Date(2024, 8, 28); // Mois indexé à 0 (8 = septembre)
@@ -113,7 +156,7 @@ function displayLoveMessage(month) {
   const message = loveMessages[messageIndex];
 
   // Afficher le message et l'image du mois correspondant
-  loveMessageElement.textContent = message.message;
+  loveMessageElement.innerHTML = message.message;
   daysLeftElement.textContent = `Mes: ${month} de ${totalMonths}`;
 
   const imageElement = document.createElement("img");
@@ -122,7 +165,7 @@ function displayLoveMessage(month) {
   imageElement.className = "loveImage";
 
   const commentElement = document.createElement("p");
-  commentElement.textContent = message.comment;
+  commentElement.innerHTML = message.comment;
   commentElement.className = "imageComment";
 
   // Supprimer l'ancienne image et commentaire s'ils existent
